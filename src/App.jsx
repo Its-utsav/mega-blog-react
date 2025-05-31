38b75be9e5b3ae63f96dd4fcf7c42ad1c91639a9
Its-utsav <utsavdhimmar987@gmail.com>
2025-05-31 20:15:10 +0530

    store for auth and some basic react-router and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,41 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Outlet } from "react-router";
+import { authService } from "./appwrite/services/auth";
+import { Footer, Header } from "./components";
+import { login, logout } from "./store/authSlice";
+
 function App() {
+    const [loading, setLoading] = useState(true);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        authService
+            .getUserInfo()
+            .then((user) => {
+                if (user) {
+                    dispatch(login({ user }));
+                } else {
+                    dispatch(logout());
+                }
+            })
+            .finally(() => setLoading(false));
+    }, []);
+
     return (
         <>
-            <h1>Hello World</h1>
+            <div className="flex min-h-screen flex-wrap content-between bg-gray-500">
+                <div className="block w-full">
+                    <Header />
+                    <main>
+                        {loading && (
+                            <p className="text-8xl text-green-400">Loading</p>
+                        )}
+                        <Outlet />
+                    </main>
+                    <Footer />
+                </div>
+            </div>
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
+import "./index.css";
+import store from "./store/store.js";
+import {
+    createBrowserRouter,
+    createRoutesFromElements,
+    Route,
+    RouterProvider,
+} from "react-router";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+
+const routes = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<App />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />}>
+                {/* <Login /> */}
+            </Route>
+        </Route>
+    )
+);
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <Provider store={store}>
+            <RouterProvider router={routes} />
+            {/* <App /> */}
+        </Provider>
     </StrictMode>
 );
